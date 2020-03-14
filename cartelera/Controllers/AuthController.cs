f6e3fa019dbde08320ce26daf17dc0f24b63cf2f
victor.arroyo.n <victor.arroyo.n@gmail.com>
2020-03-14 14:27:03 -0300

    subir archivos

---
+++ cartelera/Controllers/AuthController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace cartelera.Controllers
+{
+    [Route("auth")]
+    public class AuthController : Controller
+    {
+        [Route("")]
+        public IActionResult login()
+        {
+            return View();
+        }
+    }
+}
---
+++ cartelera/Controllers/DashBoardController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace cartelera.Controllers
+{
+    [Route("dashboard")]
+    public class DashBoardController : Controller
+    {
+        [Route("")]
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+    }
+}
